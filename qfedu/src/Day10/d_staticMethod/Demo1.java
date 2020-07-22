f44b82d9ae9e1125fb2903e68e801283419f5f4d
alleyCate <kopoppo@protonmail.com>
2020-07-22 19:15:52 +0800

    20200722 公司

--- qfedu/src/Day10/d_staticMethod/Demo1.java
+++ qfedu/src/Day10/d_staticMethod/Demo1.java
@@ -1,8 +1,21 @@
 package Day10.d_staticMethod;
 
+import javax.xml.namespace.QName;
+
 public class Demo1 {
     public static void main(String[] args) {
-        TestStatic testStatic = new TestStatic();
+        TestStatic ts = new TestStatic();
+
+        //The static method staticMethod() from the type TestStatic should be accessed in a static way
+        //在TestStatic类内使用static修饰的静态成员方法staticMethod应通过静态方式访问
+        //ts.staticMethod();
+        //通过类名调用没有任何的问题
+        TestStatic.staticMethod();
+
+        //静态方法中可以new对象
+        new TestStatic();
+
+        TestStatic.staticMethod();
     }
 }
 
@@ -18,6 +31,23 @@ class TestStatic {
 
     //静态成员方法
     public static void staticMethod() {
+        //静态方法中会用静态成员变量
+        num = 20;
+
+        //静态方法中使用非静态成员变量 NO
+        //Non-static field 'name' cannot be referenced from a static context
+        //不能定义一个静态引用指向一个非静态成员变量
+        //name = "岳云鹏";
+
+        //静态方法中使用非静态成员方法
+        //Non-static method 'test()' cannot be referenced from a static context
+        //不能在一个静态方法中去使用非静态成员方法;
+        //test();
+
+        //cannot be referenced from a static context
+        //在静态代码区间内不能使用this关键字
+        //this.name = "孙悦";
+
         System.out.println("静态成员方法");
     }
 

cbf1910cdfd388b2360164a49641036aa251e9e4
alleyCate <kopoppo@protonmail.com>
2020-07-27 19:01:21 +0800

    20200727 公司

--- qfedu/src/Day10/e_staticBlock/Demo2.java
+++ qfedu/src/Day10/e_staticBlock/Demo2.java
@@ -1,7 +1,11 @@
 package Day10.e_staticBlock;
 
 public class Demo2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        //强制加载指定类
+        Class.forName("Day10.e_staticBlock.Cat");
+
         System.out.println("请问Cat类在当前情况需要加载吗");
     }
 }
---
+++ qfedu/src/Day10/e_staticBlock/Demo3.java
@@ -0,0 +1,9 @@
+package Day10.e_staticBlock;
+
+public class Demo3 {
+
+    static Demo3 demo1 = new Demo3();
+
+    {}
+
+}

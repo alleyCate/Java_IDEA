4acea6457685bbf98384ad2437410c9afe90a372
alleyCate <kopoppo@protonmail.com>
2020-07-28 18:05:41 +0800

    20200727 公司

--- qfedu/src/Day10/e_staticBlock/Demo3.java
+++ qfedu/src/Day10/e_staticBlock/Demo3.java
@@ -2,8 +2,34 @@ package Day10.e_staticBlock;
 
 public class Demo3 {
 
-    static Demo3 demo1 = new Demo3();
+    public static void main(String[] args) {
+        Demo3 demo1 = new Demo3();
+    }
 
-    {}
+    Demo3 demo1 = new Demo3();
+    Demo3 demo2 = new Demo3();
+
+    {
+        //1
+        System.out.println("构造代码块");
+    }
+
+    static {
+        //2
+        System.out.println("静态代码块");
+    }
+
+    //3
+    public Demo3() {
+        System.out.println("构造方法");
+    }
 
 }
+
+/*  构造代码块
+    构造方法
+    构造代码块
+    构造方法
+    静态代码块
+    构造代码块
+    构造方法*/
---
+++ qfedu/src/Day11/a_staticc/Demo1.java
@@ -0,0 +1,23 @@
+package Day11.a_staticc;
+
+public class Demo1 {
+
+}
+
+/**
+ * 自定义数组工具类
+ * 工具类当中大多数方法，都是静态方法。
+ * @author alleyCat
+ */
+class MyArrays {
+    /**
+     * 数组工具类中，数组逆序的方法
+     *
+     * @param arr 这里需要的是一个int类型数组
+     */
+    public static void reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+
+        }
+    }
+}

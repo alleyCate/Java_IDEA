9fe2b3350e24a7297a01b7efe11d3342f74609b0
alleyCate <kopoppo@protonmail.com>
2020-12-07 17:44:50 +0800

    20201127 .。

--- qfedu/src/Day14/c_util/MyArrayList.java
+++ qfedu/src/Day14/c_util/MyArrayList.java
@@ -1,6 +1,6 @@
 package Day14.c_util;
 
-public class MyArrayList {
+public class MyArrayList<E> {
     /**
      * 准备一个底层数组，用于存储数据内容
      */
@@ -41,5 +41,40 @@ public class MyArrayList {
         }
 
         elements = new Object[initCapacity];
+    }
+
+    /*
+     * 增加方法
+     */
+
+    /**
+     * 添加元素到当前集合的末尾
+     * @param e 要求是符合泛型约束的指定数据类型
+     * @return 添加成功返回true，否则返回false
+     */
+    public boolean add(E e) {
+        //直接调用在指定下标位置添加元素的方法，只不过这里的制定下标位置就是尾插法下标位置
+        return add(size, e);
+    }
+
+
+    public boolean add(int index, E e) {
+
+    }
+
+    /*
+     * 这里需要类内使用的可以用于判定当前容量是否满足添加要求的方法，如果满足直接进入添加模式
+     * 如果不满足，需要执行grow方法，完成底层数组的扩容问题
+     */
+
+    /**
+     * 每一次添加元素，都要进行容量判定，如果满足可以进行添加操作，不满足需要进行grow方法
+     * @param minCapacity
+     */
+    private void ensureCapactiy(int minCapacity) {
+        if (minCapacity >= elements.length){
+            //完成一个底层数组的扩容方法
+            grow(minCapacity);
+        }
     }
 }

aec3cf9b29c986346fa7134e422d0bac435ef241
alleyCate <kopoppo@protonmail.com>
2021-05-08 18:13:54 +0800

    20210508

---
+++ qfedu/src/Day19/c_buffered/Demo4.java
@@ -0,0 +1,45 @@
+package Day19.c_buffered;
+
+import java.io.*;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        BufferedWriter bw = null;
+
+        try {
+            bw = new BufferedWriter(new FileWriter(new File("D:aaa/森巴口.txt")));
+
+            bw.write("冰咖啡");
+            bw.newLine();
+            bw.write("星冰乐");
+            bw.newLine();
+            bw.write("摩卡咖啡");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void bufferedReader() {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(new File("D:/aaa/1.txt")));
+
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.read());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- qfedu/src/Day19/c_buffered/Demp3.java
+++ qfedu/src/Day19/c_buffered/Demp3.java
@@ -1,15 +1,82 @@
 package Day19.c_buffered;
 
+import java.io.*;
+
 public class Demp3 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+        useBuffered();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time: " + (end - start));
 
     }
 
     public static void useBuffered() {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
 
+        try {
+            bis = new BufferedInputStream(new FileInputStream(new File("D:/aaa/5.txt")));
+            bos = new BufferedOutputStream(new FileOutputStream(new File("D:/aaa/buffered.txt")));
+
+            int content = -1;
+
+            while ((content = bis.read()) != 1) {
+                bos.write(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static void copy() {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
 
+        try {
+            fis = new FileInputStream("D:/aaa/5.txt");
+            fos = new FileOutputStream("D:/aaa/copy.txt");
+
+            int content = -1;
+
+            while ((content = fis.read()) != 1) {
+                fos.write(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

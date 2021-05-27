861c6aeed469c20b0702a19b523da60246ebbf7b
alleyCate <kopoppo@protonmail.com>
2021-05-27 18:06:51 +0800

    20210527

---
+++ qfedu/src/Day20/b_math/Demo1.java
@@ -0,0 +1,17 @@
+package Day20.b_math;
+
+/*
+ * Math工具类
+ */
+
+public class Demo1 {
+    public static void main(String[] args) {
+        // 绝对值
+        System.out.println(Math.abs(1.5));
+        System.out.println(Math.abs(-1.5));
+        System.out.println(Math.abs(5));
+        System.out.println(Math.abs(-5));
+
+        System.out.println(Math.ceil(1.1));
+    }
+}

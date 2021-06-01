f0ced363887af8e75125b00a85805b66dd8dd99b
alleyCate <kopoppo@protonmail.com>
2021-06-01 19:17:02 +0800

    20210531

--- qfedu/src/Day20/c_date/Demo4.java
+++ qfedu/src/Day20/c_date/Demo4.java
@@ -1,7 +1,14 @@
 package Day20.c_date;
 
+import java.util.Calendar;
+
 public class Demo4 {
     public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
 
+        calendar.add(Calendar.DAY_OF_MONTH, 2);
+        calendar.add(Calendar.YEAR, -2);
+
+        System.out.println(calendar.getTime());
     }
 }

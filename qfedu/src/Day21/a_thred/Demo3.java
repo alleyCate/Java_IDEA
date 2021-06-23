a509649ae35013208c8c692da504b00228bd3f36
alleyCate <kopoppo@protonmail.com>
2021-06-23 18:07:25 +0800

    20210622

--- qfedu/src/Day21/a_thred/Demo3.java
+++ qfedu/src/Day21/a_thred/Demo3.java
@@ -13,6 +13,9 @@ public class Demo3 {
             }
         }, "测试线程");
 
+        // main线程
+        Thread currentThread = Thread.currentThread();
+
         System.out.println(thread.getName());
         thread.setName("名称线程");
         System.out.println(thread.getName());
@@ -35,14 +38,23 @@ class MyThread3 implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
+            // MyThread3线程
+            Thread currentThread = Thread.currentThread();
+            try {
+                /*
+                 * 为什么这里只能捕获异常，不能抛出？
+                 * run方法是实现/重写Runnable接口/Thread类方法
+                 * 重写方法有一个要求，方法的声明必须和接口或者说父类一致
+                 * Runnable接口和Thread类内run方法都没有进行任何的抛出操作
+                 * 子类中重写run方法也不能抛出，保障声明一致。
+                 *
+                 * @Override注解，要求代码严格格式检查！
+                 */
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("自定义线程");
-        }
-
-        try {
-            // 为什么这里只能捕获异常，不能抛出
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 }

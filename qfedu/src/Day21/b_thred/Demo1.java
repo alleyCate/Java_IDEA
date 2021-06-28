822f52573c1a5b797ab67795e291dcfb1ad81b04
alleyCate <kopoppo@protonmail.com>
2021-06-28 18:07:51 +0800

    20210628

--- qfedu/src/Day21/b_thred/Demo1.java
+++ qfedu/src/Day21/b_thred/Demo1.java
@@ -8,7 +8,11 @@ public class Demo1 {
     public static void main(String[] args) {
         Thread thread1 = new Thread(new SingleThread(), "淘票票");
         Thread thread2 = new Thread(new SingleThread(), "猫眼");
-        Thread thread3 = new Thread(new SingleThread(), "淘票票");
+        Thread thread3 = new Thread(new SingleThread(), "美图");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
 
@@ -24,6 +28,12 @@ class SingleThread implements Runnable {
     @Override
     public void run() {
         while(true) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             if (ticket > 0) {
                 System.out.println(Thread.currentThread().getName() + "售出了" + ticket + "张票");
                 ticket -= 1;
